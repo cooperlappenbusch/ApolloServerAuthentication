a044c3dcba8a2ab63e199f6e119bba4f3518cdbd
cooperlappenbusch <coopertrooper7@gmail.com>
2022-01-29 09:52:58 -0800

    Deleted error causing messages requrie statement

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,13 +1,10 @@
-const messagesResolvers = require('./messages');
 const usersResolvers = require('./users');
 
 module.exports = {
     Query: {
-        ...messagesResolvers.Query,
         ...usersResolvers.Query
     },
     Mutation: {
-        ...messagesResolvers.Mutation,
         ...usersResolvers.Mutation
     },
 };
