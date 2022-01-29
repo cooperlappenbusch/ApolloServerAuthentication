bff6352735a2adbb00460979eb49efb9a9b9fa90
cooperlappenbusch <coopertrooper7@gmail.com>
2022-01-29 09:51:22 -0800

    Removed Messages code (not relevant to login)

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,22 +1,11 @@
 const { gql } = require('apollo-server');
 
 module.exports = gql`
-type Message {
-    text: String
-    createdAt: String
-    createdBy: String
-}
-
 type User {
     username: String,
     email: String,
     password: String,
     token: String
-}
-
-input MessageInput {
-    text: String
-    username: String
 }
 
 input RegisterInput {
@@ -31,12 +20,10 @@ input LoginInput {
 }
 
 type Query {
-    message(id: ID!): Message
     user(id: ID!): User
 }
 
 type Mutation {
-    createMessage(messageInput: MessageInput): Message!
     registerUser(registerInput: RegisterInput): User
     loginUser(loginInput: LoginInput): User
 }
--- models/Message.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require('mongoose');
-
-const messageSchema = new Schema({
-    text: String,
-    createdAt: String,
-    createdBy: String
-});
-
-module.exports = model('Message', messageSchema);
