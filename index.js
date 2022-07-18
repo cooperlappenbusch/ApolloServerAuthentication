45c4e7082dd153243d40c475024366c9b203540b
cooperlappenbusch <coopertrooper7@gmail.com>
2022-07-17 17:55:21 -0700

    Changed to node

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = new ApolloServer({
 mongoose.connect(MONGODB, {useNewUrlParser: true})
     .then(() => {
         console.log("MongoDB Connected");
-        return server.listen({port: 4000});
+        return server.listen({port: process.env.PORT || 4000});
     })
     .then((res) => {
         console.log(`Server running at ${res.url}`)
